4b3a55f54b09f3845f02cf1f5708cda38ab2b8dd
Kunaala <kunaalmalodhakar@gmail.com>
2019-07-30 10:21:55 +0530

    codespart2

---
+++ exceedingrange.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    bool a=0;
+    char b;
+    for(a=0;a<=5;a++)
+        {
+            std::cout<<a<<endl;
+        
+        }
+    for(b=0;b<=225;b++)
+        {
+            std::cout<<a<<endl;
+        
+        }
+    return 0;
+}
